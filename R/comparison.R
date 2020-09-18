9abe24eb774a5ab58d19ac0c54ef20bc5283e47f
pierredenelle <pierre.denelle@gmail.com>
2020-09-18 15:21:48 +0100

    comparison() improved: returns 2 objects, vignette improved; main figure improved

--- R/comparison.R
+++ R/comparison.R
@@ -1,16 +1,19 @@
 
-comparison <- function(dat, bio_col, thres = 10){
+comparison <- function(dat, site, bio_col, thres = 10, output = "both"){
 
+  ## 1. Controls ----
   if(!is.data.frame(dat)){
     stop("Input must be a data.frame with a column containing the sites and
          columns containing different partitions.")
   }
 
-  if(!("site" %in% colnames(dat))){
-    stop("dat must contains a column 'site' with all the sites.")
+  if(output %in% c("common", "both")){
+    if(!(site %in% colnames(dat))){
+      stop("dat must contains a column 'site' with all the sites.")
+    }
   }
 
-  if(!is.numeric(bio_col) & bio_col > ncol(dat)){
+  if(!is.numeric(bio_col) | max(bio_col) > ncol(dat)){
     stop("bio_col must be a numeric indicating which columns of dat contains
          the different bioregions.")
   }
@@ -20,64 +23,66 @@ comparison <- function(dat, bio_col, thres = 10){
          per pair.")
   }
 
+  if(!(output %in% c("percentage", "common", "both"))){
+    stop("output must be a character string indicating whether the output is
+         is a square matrix with the number of times each pair of sites have
+         been classed together ('percentage'), a data frame with the sites all
+         the time classed together ('common') or both objects ('both').")
+  }
+
+  ## 2. Groups of sites ----
   # Get matrix with number of bioregions in common per pair of pixels
   list_mat_pair <- list()
   for(i in 1:length(bio_col)){
+    # matrix with sites in rows & columns
+    # TRUE when both sites have same bioregion
     list_mat_pair[[i]] <-
       matrix(outer(dat[, bio_col[i]], dat[, bio_col[i]], "=="),
-             nrow = nrow(dat), dimnames = list(dat$site, dat$site))
+             nrow = nrow(dat), dimnames = list(dat[, site], dat[, site]))
   }
-  # Sum all matrices
+  # Sum all matrices: how many times each pair of sites has same bioregion
   list_mat_pair <- Reduce('+', list_mat_pair)
 
-  # Get percentage
-  list_mat_pair <- 100*list_mat_pair/length(bio_col)
+  # Remove lower part and diagonal of the matrix
+  diag(list_mat_pair) <- NA
+  list_mat_pair[lower.tri(list_mat_pair)] <- NA
 
-  # Conversion to data frame
-  list_df_pair <- as.data.frame(as.table(list_mat_pair))
-  colnames(list_df_pair) <- c("id1", "id2", "perc")
+  if(output == "percentage"){
+    return(list_mat_pair)
+  } else if(output %in% c("common", "both")){
 
-  # Remove false pairs of sites
-  list_df_pair <- list_df_pair[which(list_df_pair$id1 != list_df_pair$id2), ]
+    # percentage instead of sum
+    list_mat_pair <- 100*list_mat_pair/length(bio_col)
 
-  # Extract pixels together 100% of the time
-  all100 <- list_df_pair[which(list_df_pair$perc == 100), ]
+    # Conversion to data frame
+    list_df_pair <- reshape2::melt(list_mat_pair)
+    colnames(list_df_pair) <- c("id1", "id2", "perc")
 
-  # Add bioregion (whatever algorithm since all pixels are grouped together)
-  all100 <- left_join(all100,
-                      all_bioregions[, c("site",
-                                         colnames(all_bioregions)[bio_col])],
-                      by = c("id1" = "site"))
-  colnames(all100) <- c("id1", "id2", "perc",
-                        paste0(colnames(all_bioregions)[bio_col], "_id1"))
+    # Remove NAs
+    list_df_pair <- list_df_pair[complete.cases(list_df_pair), ]
 
-  all100 <- left_join(all100,
-                      all_bioregions[, c("site",
-                                         colnames(all_bioregions)[bio_col])],
-                      by = c("id2" = "site"))
-  colnames(all100) <- c("id1", "id2", "perc",
-                        paste0(colnames(all_bioregions)[bio_col], "_id1"),
-                        paste0(colnames(all_bioregions)[bio_col], "_id2"))
+    # Extract pixels together 100% of the time
+    all100 <- list_df_pair[which(list_df_pair$perc == 100), ]
 
-  bioregion_cols <- c(paste0(colnames(all_bioregions)[bio_col], "_id1"),
-                      paste0(colnames(all_bioregions)[bio_col], "_id2"))
+    # Add bioregion (whatever algorithm since all pixels are grouped together)
+    all100 <- left_join(all100,
+                        dat[, c(site, colnames(dat)[bio_col[1]])],
+                        by = c("id1" = site))
+    colnames(all100) <- c("id1", "id2", "perc", "uni")
 
-  # all100$pair_bio <- paste(all100$bio_id1, all100$bio_id2, sep = "_")
+    # Rename levels of common bioregion
+    all100$uni <- as.factor(all100$uni)
+    levels(all100$uni) <- as.character(seq(1:length(levels(all100$uni))))
 
-  # all100$pair_bio <- tidyr::unite_(data,
-  #                                  paste(bioregion_cols, collapse = "_"),
-  #                                  bioregion_cols)
-  all100$pair_bio <- apply(all100[, bioregion_cols] , 1, paste, collapse = "_")
+    # Remove pairs of pixels under a given threshold of pixels
+    thresh <- names(table(all100$uni)[table(all100$uni) > thres])
 
-  # Add a column with easier names
-  all100$uni <- as.factor(all100$pair_bio)
-  levels(all100$uni) <- as.character(seq(1:length(levels(all100$uni))))
+    all100 <- all100[which(all100$uni %in% thresh), ]
 
-  # Remove pairs of pixels under a threshold of 10 pixels
-  thresh <- names(table(all100$pair_bio)[table(all100$pair_bio) > thres])
-
-  all100 <- all100[which(all100$pair_bio %in% thresh), ]
-
-  # Output: data frame with pixels grouped together through all the methods
-  return(all100)
+    if(output == "common"){
+      return(all100)
+    } else if(output == "both"){
+      return(list(list_mat_pair, all100))
+    }
+  }
 }

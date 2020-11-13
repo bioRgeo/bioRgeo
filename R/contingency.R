251d2df095e35a6c7ab4f27b299c1c3ab22d299a
pierredenelle <pierre.denelle@gmail.com>
2020-11-13 14:58:06 +0100

    Clearer arguments in contingency() and contingency_to_df(), help page and vignette updated.

--- R/contingency.R
+++ R/contingency.R
@@ -1,49 +1,40 @@
 
-contingency <- function(dat, site, sp, ab = NULL, weight = FALSE){
+contingency <- function(dat, site.col, species.col, abundance.col = NULL){
 
   if(!is.data.frame(dat)){
-    stop("dat must be a data.frame with columns sp and site.")
+    stop("dat must be a data.frame with columns species and sites.")
   }
 
-  if(!is.character(site)){
-    stop("site must be the column name of dat describing the sites.")
+  if(!is.character(site.col)){
+    stop("site.col must be the column name of dat describing the sites.")
   }
 
-  if(!is.character(sp)){
-    stop("sp must be the column name of dat describing the species.")
+  if(!is.character(species.col)){
+    stop("species.col must be the column name of dat describing the species.")
   }
 
-  if(!is.null(ab) & !is.character(ab)){
-    stop("ab must be the column name of dat describing the abundances
-         of species.")
-  }
-
-  if(is.null(ab) & weight == TRUE){
-    warning("Without column abundances, contingency table will only get binary
-        values.")
-  }
-
-  if(!is.logical(weight)){
-    stop("weight must be a boolean.")
+  if(!is.null(abundance.col) & !is.character(abundance.col)){
+    stop("abundance.col must be the column name of dat describing the
+    abundances of species.")
   }
 
   # Rename columns
-  colnames(dat)[colnames(dat) == sp] <- "sp"
-  colnames(dat)[colnames(dat) == site] <- "site"
-  if(!is.null(ab)){
-    colnames(dat)[colnames(dat) == ab] <- "ab"
+  colnames(dat)[colnames(dat) == species.col] <- "species.col"
+  colnames(dat)[colnames(dat) == site.col] <- "site.col"
+  if(!is.null(abundance.col)){
+    colnames(dat)[colnames(dat) == abundance.col] <- "abundance.col"
   } else{ # without abundances, species are just present in the sites
-    dat$ab <- 1
+    dat$abundance.col <- 1
   }
 
   # Create contingency table with matrix indexing
-  dat$site <- as.factor(dat$site)
-  dat$sp <- as.factor(dat$sp)
+  dat$site.col <- as.factor(dat$site.col)
+  dat$species.col <- as.factor(dat$species.col)
 
   mat <- with(dat, {
-    out <- matrix(nrow = nlevels(site), ncol = nlevels(sp),
-                  dimnames = list(levels(site), levels(sp)))
-    out[cbind(site, sp)] <- ab
+    out <- matrix(nrow = nlevels(site.col), ncol = nlevels(species.col),
+                  dimnames = list(levels(site.col), levels(species.col)))
+    out[cbind(site.col, species.col)] <- abundance.col
     out
   })
   # Replace NAs with 0s
@@ -53,8 +44,8 @@ contingency <- function(dat, site, sp, ab = NULL, weight = FALSE){
   mat <- mat[rowSums(mat) > 0, ]
   mat <- mat[, colSums(mat) > 0]
 
-  # Conversion as binary matrix if weight == FALSE
-  if(weight == FALSE){
+  # Conversion as binary matrix if is.null(abundance.col)
+  if(is.null(abundance.col)){
     mat[mat > 0] <- 1
   }
 
--- R/contingency_to_df.R
+++ R/contingency_to_df.R
@@ -1,38 +1,37 @@
 
-contingency_to_df <- function(dat, col1 = "site", col2 = "sp",
-                                     col3 = "ab", remove_zeros = TRUE){
-  require(DescTools)
-
+contingency_to_df <- function(dat, site.col = "site", species.col = "species",
+                              abundance.col = "abundances",
+                              remove_absent_species = TRUE){
   if(!is.matrix(dat)){
     stop("Contingency table should be a matrix with sites as rows and
          species as columns.")
   }
 
-  if(!is.character(col1)){
-    stop("col1 must be the column name describing the rows of the contingency
+  if(!is.character(site.col)){
+    stop("site.col must be the column name describing the rows of the contingency
          matrix.")
   }
 
-  if(!is.character(col2)){
-    stop("col2 must be the column name describing the columns of the
+  if(!is.character(species.col)){
+    stop("species.col must be the column name describing the columns of the
     contingency matrix.")
   }
 
-  if(!is.character(col3)){
-    stop("col3 must be the column name describing the values of the contingency
-         matrix.")
+  if(!is.character(abundance.col)){
+    stop("abundance.col must be the column name describing the values of the
+    contingency matrix.")
   }
 
-  if(!is.logical(remove_zeros)){
-    stop("remove_zeros must be a boolean determining whether the null values
-         from the contingency matrix has to be removed from the output.")
+  if(!is.logical(remove_absent_species)){
+    stop("remove_absent_species must be a boolean determining whether absent
+    species from the contingency matrix have to be removed from the output.")
   }
 
   # Conversion as data.frame
   dat_df <- reshape2::melt(dat)
-  colnames(dat_df) <- c(col1, col2, col3)
+  colnames(dat_df) <- c(site.col, species.col, abundance.col)
 
-  if(remove_zeros == TRUE){
+  if(remove_absent_species == TRUE){
     dat_df <- dat_df[which(dat_df$ab > 0), ]
   }
 
